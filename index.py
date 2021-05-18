3c7d002058e42a4fa45236ad3c0d34ac85caf4d0
Sanjeev Gupta <guptasanjeev876@gmail.com>
2021-05-18 11:47:59 +0530

    second commit

--- index.py
+++ index.py
@@ -1,11 +1,14 @@
 import requests
 import bs4
-import webbrowser
-from bs4 import BeautifulSoup 
+
 
 url= "https://prefeitura.pbh.gov.br/saude/licitacao/pregao-eletronico-151-2020"
-r = requests.get(url)
+data = requests.get(url)
 
-soup = bs4.BeautifulSoup(r.content, 'html.parser')
+soup = bs4.BeautifulSoup(data.content, 'html.parser')
 
 print(soup.select(".lbl-licitacao"))
+
+for links in soup.find_all('a'):
+    link = links.get('href')
+    print(link)
